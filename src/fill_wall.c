62c28c5748d48066af29248974afc56af2c6b295
Quentin <quentin.gimenez@ecole-89.com>
2023-01-25 16:04:50 +0100

    runner save

--- src/fill_wall.c
+++ src/fill_wall.c
@@ -15,20 +15,21 @@ void fill_wall(struct display *ds, unsigned int color)
 {
     int width;
     int height;
-    unsigned int *pix;
     t_accurate_pos end;
+    t_bunny_position pos;
 
     width  = ds->ds_px->clipable.clip_width;
     height = ds->ds_px->clipable.clip_height;
-    pix    = (unsigned int *) ds->ds_px->pixels;
     end.y = height;
-    while (end.y > 0) {
+    while (end.y >= 0) {
         end.x = width;
-        while (end.x > 0) {
-            if (ds->map.map[(((int)end.y / 100) * ds->map.width) + ((int)end.x / 100)] == 1) {
-                pix[(int)end.x * (int)end.y] = color;
+        while (end.x >= 0) {
+            pos = pos_from_accurate(&end);
+            if (ds->map.map[(((int)end.y / 100) * ds->map.width) +
+                            ((int)end.x / 100)] == 1) {
+                put_pixel(ds->ds_px, &pos, color);
             } else {
-                pix[(int)end.x * (int)end.y] = BLACK;
+                put_pixel(ds->ds_px, &pos, BLACK);
             }
             end.x -= 1;
         }
